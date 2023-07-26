3a97d58b124612ba4b4072c571ae7088dd68bb24
Goutte-de-mer <114480998+Goutte-de-mer@users.noreply.github.com>
2023-07-26 16:53:46 +0200

    Add files via upload

--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+# Créer une instance de l'application Flask
+def create_app():
+    app = Flask(__name__)
 
-# Configuration de la base de données MySQL
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://root:root@localhost:8889/db_alpha_breakfast"
-# Vous pouvez également désactiver le suivi des modifications pour améliorer les performances (optionnel)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    # Configuration de la base de données MySQL
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "mysql+pymysql://root:root@localhost:8889/db_alpha_breakfast"
+    # Vous pouvez également désactiver le suivi des modifications pour améliorer les performances (optionnel)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# Initialisation de l'extension SQLAlchemy
-db = SQLAlchemy(app)
+    # Initialisation de l'extension SQLAlchemy
+    db = SQLAlchemy(app)
+
+    # Vous pouvez importer et enregistrer vos modèles ici, par exemple :
+    # from .models import breakfast, reservations, users
+
+    return app
