2b374d6f28a6ee0dd3d3e5ece91719e27c3f224c
Goutte-de-mer <114480998+Goutte-de-mer@users.noreply.github.com>
2023-07-26 17:56:43 +0200

    affichage places restantes (ne marche pas)

--- src/routes.py
+++ src/routes.py
@@ -1,17 +1,22 @@
-from models import breakfast
 from flask import render_template
+from models import breakfast, reservations
 from src import create_app
-from datetime import datetime
 
 app = create_app()
 
 
 @app.route("/", methods=["GET"])
 def index():
-    # Récupérez toutes les dates disponibles depuis la table "breakfast"
-    dates = [
-        datetime.strptime(str(b.date), "%Y-%m-%d").strftime("%d-%m-%Y")
-        for b in breakfast.query.all()
-    ]
+    # Get all breakfast dates and quotas
+    breakfasts = breakfast.query.all()
 
-    return render_template("index.html", dates=dates)
+    # Calculate the remaining places for each breakfast date
+    dates_and_remaining_places = {}
+    for b in breakfasts:
+        total_people = reservations.query.filter_by(id_breakfast=b.id).count()
+        remaining_places = b.quota - total_people
+        dates_and_remaining_places[b.date.strftime("%d-%m-%Y")] = remaining_places
+
+    return render_template(
+        "index.html", dates_and_remaining_places=dates_and_remaining_places
+    )
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,22 +1,31 @@
-// Fonction pour récupérer les dates disponibles depuis le backend
-function fetchDates() {
-  axios
-    .get("/")
-    .then((response) => {
-      const dates = response.data.dates;
-      const dateSelect = document.getElementById("date-select");
-      // Ajouter les options pour chaque date disponible dans la liste déroulante
-      dates.forEach((date) => {
-        const option = document.createElement("option");
-        option.value = date;
-        option.text = date;
-        dateSelect.appendChild(option);
-      });
-    })
-    .catch((error) => {
-      console.error("Erreur lors de la récupération des dates :", error);
-    });
+// Get the select element
+const dateSelect = document.getElementById("date-select");
+
+// JSON string of dates_and_remaining_places variable from Flask
+const remainingPlacesData = `{{ dates_and_remaining_places | tojson | safe }}`;
+
+// Convert the JSON string to a JavaScript object
+const remainingPlacesObj = JSON.parse(remainingPlacesData);
+
+// Function to update the remaining places
+function updateRemainingPlaces() {
+  // Get the selected date from the select element
+  const selectedDate = dateSelect.value;
+
+  // Split the selected date into day, month, and year components
+  const [day, month, year] = selectedDate.split("-");
+
+  // Get the remaining places for the selected date from the JavaScript object
+  const remainingPlaces = remainingPlacesObj[selectedDate];
+
+  // Update the content in the 'remaining-places-info' element with the desired format
+  document.getElementById(
+    "remaining-places-info"
+  ).textContent = `Remaining places for ${day}-${month}-${year}: ${remainingPlaces}`;
 }
 
-// Appeler la fonction pour récupérer les dates au chargement de la page
-window.onload = fetchDates;
+// Call the function to update the remaining places when the user changes the date selection
+dateSelect.addEventListener("change", updateRemainingPlaces);
+
+// Call the function initially to display the remaining places for the default selected date
+updateRemainingPlaces();
